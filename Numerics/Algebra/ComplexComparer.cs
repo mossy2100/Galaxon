921d10c99623d8303c207514cde367e8281540aa
Shaun Moss <shaun@astromultimedia.com>
2024-03-01 18:26:40 +1000

    Added documentation for ComplexComparer.cs

--- Numerics/Algebra/ComplexComparer.cs
+++ Numerics/Algebra/ComplexComparer.cs
@@ -4,9 +4,20 @@ namespace Galaxon.Numerics.Algebra;
 
 public class ComplexComparer : IComparer<Complex>
 {
+    /// <summary>
+    /// Compare 2 Complex values.
+    /// This does not compare magnitudes of the complex numbers, as the method would then show
+    /// different values as being equal.
+    /// So, the method is for the purpose of ordering values in a list, rather than comparing
+    /// magnitudes.
+    /// First the real parts are compared, then if they are equal, the imaginary parts are compared.
+    /// </summary>
+    /// <param name="x">The first Complex value.</param>
+    /// <param name="y">The second Complex value.</param>
+    /// <returns>If one is less than the other for the purpose of ordering.</returns>
     public int Compare(Complex x, Complex y)
     {
-        // First we consider only the real parts.
+        // First we consider the real parts.
         if (x.Real < y.Real)
         {
             return -1;
