c2af5fd9b7113a9990435c120f8e5e46ddcb4933
Shaun Moss <shaun@astromultimedia.com>
2024-09-15 16:49:17 +1000

    Made AddNode return the new Node. Made AddEdge return the new Edge.

--- Numerics/Algorithms/Dijkstra/Edge.cs
+++ Numerics/Algorithms/Dijkstra/Edge.cs
@@ -3,6 +3,17 @@ namespace Galaxon.Numerics.Algorithms.Dijkstra;
 public class Edge
 {
     /// <summary>
+    /// If this is true, the edge can be traversed in both directions.
+    /// If this is false, the edge can only be traversed in one direction, from node1 to node2.
+    /// </summary>
+    public readonly bool bidirectional;
+
+    /// <summary>
+    /// The distance between the two nodes.
+    /// </summary>
+    public readonly double distance;
+
+    /// <summary>
     /// The start node.
     /// </summary>
     public readonly Node node1;
@@ -11,17 +22,6 @@ public class Edge
     /// The finish node.
     /// </summary>
     public readonly Node node2;
-
-    /// <summary>
-    /// The distance between the two nodes.
-    /// </summary>
-    public readonly double distance;
-
-    /// <summary>
-    /// If this is true, the edge can be traversed in both directions.
-    /// If this is false, the edge can only be traversed in one direction, from node1 to node2.
-    /// </summary>
-    public readonly bool bidirectional;
 
     public Edge(Node node1, Node node2, double distance, bool bidirectional = false)
     {
--- Numerics/Algorithms/Dijkstra/Graph.cs
+++ Numerics/Algorithms/Dijkstra/Graph.cs
@@ -3,14 +3,14 @@ namespace Galaxon.Numerics.Algorithms.Dijkstra;
 public class Graph
 {
     /// <summary>
-    /// The graph's nodes.
-    /// </summary>
-    public List<Node> nodes = [];
-
-    /// <summary>
     /// The graph's edges.
     /// </summary>
     public List<Edge> edges = [];
+
+    /// <summary>
+    /// The graph's nodes.
+    /// </summary>
+    public List<Node> nodes = [];
 
     /// <summary>
     /// Get the node with a given label.
@@ -23,10 +23,13 @@ public class Graph
     }
 
     /// <summary>
-    /// Add a new node to the graph.
+    /// Adds a new node to the graph.
     /// </summary>
-    /// <param name="label"></param>
-    public void AddNode(string label)
+    /// <param name="label">The label of the node to be added.</param>
+    /// <returns>The newly added node.</returns>
+    /// <exception cref="ArgumentException">Thrown when a node with the provided label already exists in
+    /// the graph.</exception>
+    public Node AddNode(string label)
     {
         // Guard.
         if (GetNode(label) != null)
@@ -35,7 +38,11 @@ public class Graph
         }
 
         // Create and add the new node.
-        nodes.Add(new Node(label));
+        Node newNode = new (label);
+        nodes.Add(newNode);
+
+        // Return the new node.
+        return newNode;
     }
 
     /// <summary>
@@ -44,9 +51,12 @@ public class Graph
     /// <param name="node1">The label of the first node of the edge.</param>
     /// <param name="node2">The label of the second node of the edge.</param>
     /// <param name="distance">The distance between the two nodes.</param>
-    /// <param name="bidirectional">A boolean indicating whether the edge is bidirectional. Default is false.</param>
-    /// <exception cref="ArgumentException">Thrown when a node with the provided label is not found in the graph.</exception>
-    public void AddEdge(string node1, string node2, double distance, bool bidirectional = false)
+    /// <param name="bidirectional">A boolean indicating whether the edge is bidirectional. Default is
+    /// false.</param>
+    /// <returns>The newly added edge.</returns>
+    /// <exception cref="ArgumentException">Thrown when a node with the provided label is not found in the
+    /// graph.</exception>
+    public Edge AddEdge(string node1, string node2, double distance, bool bidirectional = false)
     {
         // Check if the nodes exist.
         Node? n1 = GetNode(node1);
@@ -61,7 +71,11 @@ public class Graph
         }
 
         // Add the edge.
-        edges.Add(new Edge(n1, n2, distance, bidirectional));
+        Edge newEdge = new (n1, n2, distance, bidirectional);
+        edges.Add(newEdge);
+
+        // Return the new Edge.
+        return newEdge;
     }
 
     /// <summary>
@@ -69,13 +83,18 @@ public class Graph
     /// If the end node is null, it finds the shortest path to all nodes from the start node.
     /// </summary>
     /// <param name="startNodeLabel">The label of the start node.</param>
-    /// <param name="endNodeLabel">The label of the end node. If null, the method will find the shortest path to all nodes.</param>
-    /// <exception cref="ArgumentException">Thrown when the start node or end node is not found in the graph.</exception>
+    /// <param name="endNodeLabel">The label of the end node. If null, the method will find the shortest
+    /// path to all nodes.</param>
+    /// <exception cref="ArgumentException">Thrown when the start node or end node is not found in the
+    /// graph.</exception>
     public void ShortestPath(string startNodeLabel, string? endNodeLabel = null)
     {
         // Get the start and end nodes.
-        Node startNode = GetNode(startNodeLabel) ?? throw new ArgumentException("Start node not found.");
-        Node? endNode = endNodeLabel == null ? null : (GetNode(endNodeLabel) ?? throw new ArgumentException("End node not found."));
+        Node startNode = GetNode(startNodeLabel)
+            ?? throw new ArgumentException("Start node not found.");
+        Node? endNode = endNodeLabel == null
+            ? null
+            : GetNode(endNodeLabel) ?? throw new ArgumentException("End node not found.");
 
         // If the start node distance hasn't been set, set it to 0.
         if (double.IsPositiveInfinity(startNode.distanceFromStart))
@@ -97,8 +116,8 @@ public class Graph
             }
 
             // Get the edges leading from the current node.
-            IEnumerable<Edge> edgesToNeighbour = edges
-                .Where(e => e.node1 == currentNode || (e.bidirectional && e.node2 == currentNode));
+            IEnumerable<Edge> edgesToNeighbour = edges.Where(e =>
+                e.node1 == currentNode || (e.bidirectional && e.node2 == currentNode));
 
             // Traverse the edges.
             foreach (Edge edge in edgesToNeighbour)
